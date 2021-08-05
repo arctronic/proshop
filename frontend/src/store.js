ff4d3e5c31f1e8a3e1e4ac68ee89d15cc2214527
arctronic <merajuli46@gmail.com>
2021-08-05 15:41:31 +0600

    redux for login

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,17 +4,24 @@ import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer,productDetailsReducer } from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducer'
+import {userLogInReducer} from './reducers/userReducer'
+
 
 const reducer = combineReducers({
 	productList : productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
+	userLogin: userLogInReducer,
 })
 
 const cartItemFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) :[]
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) :null
 
 
-const initialState = {cartItems: cartItemFromStorage}
+const initialState = {
+	cart: {cartItems: cartItemFromStorage},
+	userLogin: {userInfo: userInfoFromStorage}
+}
 const middleware = [thunkMiddleware]
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
